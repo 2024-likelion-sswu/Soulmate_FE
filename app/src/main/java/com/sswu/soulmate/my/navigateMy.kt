bf4400b734d8c69cb4f5c4ceb1fc09a6233ed4d9
hyeumm <20221159@sungshin.ac.kr>
2025-01-08 03:50:23 +0900

    Chore: add new navigate route

--- app/src/main/java/com/sswu/soulmate/my/navigateMy.kt
+++ app/src/main/java/com/sswu/soulmate/my/navigateMy.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.sswu.soulmate.main.TabRoute
+import com.sswu.soulmate.my.RegistRoute
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateMy(
@@ -14,13 +15,26 @@ fun NavController.navigateMy(
     navigate(My, navOptions)
 }
 
+fun NavController.navigateRegist(
+    navOptions: NavOptions
+) {
+    navigate(Regist, navOptions)
+}
+
 fun NavGraphBuilder.myNavGraph(
     paddingValues: PaddingValues,
+    navigateToMy: () -> Unit
 ) {
     composable<My> {
         MyRoute(paddingValues = paddingValues)
+    }
+    composable<Regist> {
+        RegistRoute(paddingValues = paddingValues, navigateToMy = navigateToMy)
     }
 }
 
 @Serializable
 data object My : TabRoute
+
+@Serializable
+data object Regist : TabRoute
