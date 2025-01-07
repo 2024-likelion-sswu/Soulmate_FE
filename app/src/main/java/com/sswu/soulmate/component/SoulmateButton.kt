62622481143000c81dd609442856f50c5ac31043
hyeumm <20221159@sungshin.ac.kr>
2025-01-07 22:18:27 +0900

    Feature: core component - button

---
+++ app/src/main/java/com/sswu/soulmate/component/SoulmateButton.kt
@@ -0,0 +1,35 @@
+package com.sswu.soulmate.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sswu.soulmate.R
+
+@Composable
+fun SoulmateButton(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        style = TextStyle(
+            fontFamily = FontFamily(Font(R.font.pretendard_bold)),
+            fontSize = 18.sp,
+            color = White,
+            textAlign = TextAlign.Center
+        ),
+        modifier = modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(DarkGray)
+            .padding(vertical = 5.dp, horizontal = 15.dp)
+    )
+}
